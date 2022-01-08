6a6c9d9a7170b856af336f0dba0dce8efb5b094b
Kadirds <61584943+Kadirds@users.noreply.github.com>
2022-01-08 20:21:43 +0300

    GameManager State'ler eklendi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,4 +6,45 @@ public class GameManager : MonoBehaviour
 {
     public GameManager StartP, InGameP, NextP, GameOverP;
     public float CountDown = 2F;
+
+    [SerializeField] private int asynSceneIndex = 1;
+
+    public enum GameState{
+        
+        Start,
+        Ingame,
+        Next,
+        Gameover
+
+
+    }
+    public GameState gamestate;
+    public enum Panels
+    {
+        Startp,
+        Ingame,
+        Nextp,
+        Gameoverp,
+    }
+    private void Start()
+    {
+        gamestate = GameState.Start;
+    }
+    private void Update()
+    {
+        switch (gamestate)
+        {
+            case GameState.Start:
+                break;
+
+            case GameState.Ingame:
+                break;
+
+            case GameState.Next:
+                break;
+
+            case GameState.Gameover:
+                break;
+        }
+    }
 }
